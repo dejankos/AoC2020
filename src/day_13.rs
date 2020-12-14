5f21a94f6a1820478ac1b3139e9d8dee305f7e12
dejankos <kosdejan@yahoo.com>
2020-12-14 21:19:00 +0100

    day 13 part 1 imp

--- src/day_13.rs
+++ src/day_13.rs
@@ -3,15 +3,14 @@ use std::collections::HashMap;
 fn solve(input: (usize, Vec<usize>)) -> usize {
     let ts = input.0;
 
-    let map: HashMap<usize, usize> = input
+    let min = input
         .1
         .into_iter()
         .map(|id| (find_first(id, ts), id))
-        .collect();
+        .min_by_key(|p| p.0)
+        .unwrap();
 
-    let min = map.keys().into_iter().min().unwrap();
-
-    (min - ts) * map[min]
+    (min.0 - ts) * min.1
 }
 
 fn find_first(id: usize, ts: usize) -> usize {
@@ -34,6 +33,16 @@ fn prepare_data(data: Vec<String>) -> (usize, Vec<usize>) {
         .collect();
 
     (ts, ids)
+}
+
+fn prepare_data_part_2(data: Vec<String>) -> Vec<(usize, usize)> {
+    data[1]
+        .split(',')
+        .into_iter()
+        .enumerate()
+        .filter(|(_, s)| *s != "x")
+        .map(|(i, s)| (s.parse::<usize>().unwrap(), i))
+        .collect()
 }
 
 #[cfg(test)]
